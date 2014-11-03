489fa1267717b8371eddfbc317255636ea9196b9
Scott Burbidge <scottisreally@gmail.com>
2014-11-03 12:24:17 -0800

    added ;

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 'use strict'
 
 var fs = require('fs');
-var wr = fs.createWriteStream(process.argv[3])
+var wr = fs.createWriteStream(process.argv[3]);
 
-wr.write(process.argv[2])
+wr.write(process.argv[2]);
 
-process.stdin.pipe(wr)
-process.stdin.pipe(process.stdout)
+process.stdin.pipe(wr);
+process.stdin.pipe(process.stdout);
 
-console.log("user input stored in stdout")
+console.log("user input stored in stdout");
 
 
